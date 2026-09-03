887922c4a084f99be1d6ced0fd8fc63a5f2c8da9
danphe1 <62278214+danphe1@users.noreply.github.com>
2026-09-04 01:44:19 +0545

    build: add official Zello SDK repository

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -3,7 +3,14 @@ pluginManagement {
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories { google(); mavenCentral() }
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            url = uri("https://zello-sdk.s3.amazonaws.com/android/latest")
+            content { includeGroup("com.zello") }
+        }
+    }
 }
 rootProject.name = "NepalScoutsRescuer"
 include(":app")
